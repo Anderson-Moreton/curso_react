27824960d1d8c57531ff70ac7ea5b62cc07276e3
Anderson Moreton <anderson.moreton@gmail.com>
2023-04-08 19:08:53 -0300

    Fundamento do React.

---
+++ 3_AVANCANDO _NO_REACT/avancando/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React!</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem com por do sol" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
